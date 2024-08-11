9290dfcffb14c545210103d39fb75600ecb580c9
lucaslija <81848948+lucaslija@users.noreply.github.com>
2024-08-11 16:00:04 -0700

    image asset bundling via html/js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 // index.js
 import "./styles.css";
 import { greeting } from "./greeting.js";
+import odinImage from "./odin.png";
 
 console.log(greeting);
+
+const image = document.createElement("img");
+image.src = odinImage;
+
+document.body.appendChild(image);
