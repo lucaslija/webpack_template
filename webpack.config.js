cfd13b9ddf5e40ea9397cb155b88356c5a9c1cbe
lucaslija <81848948+lucaslija@users.noreply.github.com>
2024-08-11 15:49:06 -0700

    added css bundling

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 // webpack.config.js
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -8,5 +9,18 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
 };
