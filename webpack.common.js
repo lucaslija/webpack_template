59b673022b6889b8c5828e57ce18ea5a51ffc18a
lucaslija <81848948+lucaslija@users.noreply.github.com>
2024-08-24 18:51:15 -0700

    split webpack.config into dev, prod, common

---
+++ webpack.common.js
@@ -0,0 +1,37 @@
+// webpack.common.js
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/template.html',
+    }),
+  ],
+
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      }
+    ],
+  },
+};
---
+++ webpack.dev.js
@@ -0,0 +1,12 @@
+// webpack.dev.js
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'eval-source-map',
+  devServer: {
+    static: './dist',
+    watchFiles: ["./src/template.html"],
+  },
+});
